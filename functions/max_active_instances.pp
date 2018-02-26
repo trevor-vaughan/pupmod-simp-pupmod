bfabd22ea13ff7d63f61a4cd135507c580b285ab
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-02-25 22:32:21 -0500

    Updating for P5

---
+++ functions/max_active_instances.pp
@@ -0,0 +1,21 @@
+# Provides a reasonable calculation for the maximum number of active instances
+# for a system
+#
+# Parameters are not to be used but are present as an assist to testing
+#
+# @param memory_limited_instances
+# @param processor_count
+function pupmod::max_active_instances (
+  Integer[0] $memory_limited_instances = Integer(inline_template("<%= @facts['memorysize_mb'].to_i / 512 %>")),
+  Integer[1] $processor_count          = $facts['processors']['count']
+){
+
+  if ($memory_limited_instances < 2) or ($processor_count < 3) {
+    $max_active_instances = 1
+  }
+  else {
+    $max_active_instances = inline_template("<%= [@memory_limited_instances, @processor_count].min - 1 %>")
+  }
+
+  $max_active_instances
+}
---
+++ functions/server_version.pp
@@ -0,0 +1,21 @@
+# Authoritatively determine the puppet server version and return `undef` if one
+# could not be determined.
+#
+function pupmod::server_version {
+  # Authoritatively determine the puppet server version
+  if defined('$::serverversion') {
+    $server_version = $::serverversion
+  }
+  elsif fact('server_facts.serverversion') {
+    $server_version = fact('server_facts.serverversion')
+  }
+  elsif fact('simp_pupmod_serverversion') {
+    $server_version = fact('simp_pupmod_serverversion')
+  }
+  else {
+    $server_version = '0.0.0'
+  }
+
+  $server_version
+}
+
